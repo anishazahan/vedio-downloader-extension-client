4634f71ffa72d0fb16ca77b008840822f2bc1d68
anishazahan <anishazahan13@gmail.com>
2023-08-26 21:04:22 +0600

    fetch vedio information and show

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import axios from "axios";
 import NextTopLoader from "nextjs-toploader";
 import Loader from "@/components/home/Loader";
+import Image from "next/image";
+
+interface VideoInfo {
+  title: string;
+  thumbnailUrl: string;
+  videoResulation: string[];
+  // Add other properties as needed
+}
 
 const Download = () => {
   const [link, setLink] = useState("");
-  const [videoInfo, setVideoInfo] = useState("");
+  const [videoInfo, setVideoInfo] = useState<VideoInfo | null>(null);
   const [resulation, setResulation] = useState("");
   const [loader, setLoader] = useState(false);
 
@@ -22,17 +30,21 @@ const Download = () => {
       console.log(data);
       setLoader(false);
       setVideoInfo(data.videoInfo);
-      setResulation(data.videoInfo.lastResu);
+      console.log(videoInfo?.thumbnailUrl);
     } catch (error: any) {
       console.log(error.response);
     }
   };
 
+  const video_download = () => {
+    const videoId = link.split("https://youtu.be/")[1];
+    const url = `http://localhost:5000/video-download?id=${videoId}&resu=${resulation}`;
+    window.location.href = url;
+  };
+
   return (
     <div className="bg-gray-800 w-full h-[100vh] flex justify-center items-center">
-      {/* ... form ... */}
-
-      <div className="w-[600px] h-[500px] card-box bg-black/70 border border-opacity-40">
+      <div className="w-[700px] h-[500px] card-box bg-black/70 border border-opacity-40">
         <h2 className="text-xl text-white font-bold text-center py-8">
           Youtube Video Downloader
         </h2>
@@ -53,10 +65,50 @@ const Download = () => {
           </div>
         </form>
 
-        {/* ... download ... */}
-
         <div>
-          {loader ? <Loader></Loader> : <h2 className="text-white">hellow</h2>}
+          {loader ? (
+            <Loader />
+          ) : (
+            videoInfo && (
+              <div className="flex gap-5 px-4 pt-10 items-center">
+                <div className=" w-full lg:w-[50%]">
+                  <Image
+                    width="300"
+                    height="300"
+                    className=" rounded-md "
+                    src={videoInfo?.thumbnailUrl}
+                    alt=""
+                  />
+                </div>
+
+                <div className="text-white flex gap-2 flex-col w-full lg:w-[50%]">
+                  <h3 className="font-semibold text-xl">
+                    {videoInfo?.title.slice(0, 70)}...
+                  </h3>
+                  <span>Time : 33.43</span>
+                  <div className="flex gap-3 mt-4">
+                    <select
+                      onChange={(e) => setResulation(e.target.value)}
+                      className="px-3 py-2 outline-none bg-slate-800 border border-purple-500 pr-3 rounded"
+                    >
+                      {videoInfo?.videoResulation.length > 0 &&
+                        videoInfo?.videoResulation.map((v, i) => (
+                          <option key={i} value={v}>
+                            {v}p
+                          </option>
+                        ))}
+                    </select>
+                    <button
+                      onClick={video_download}
+                      className="px-3 py-2 bg-purple-600 text-white rounded"
+                    >
+                      Download
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )
+          )}
         </div>
       </div>
     </div>
